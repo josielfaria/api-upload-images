05023e2f562bbe6f13db2f5eb93b28e7bee73b92
Josiel Faria <developerjosiel@gmail.com>
2021-07-29 15:29:25 -0300

    Incluindo enpoint para salvar imagem

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,27 +2,36 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+  /**
+   * Register any application services.
+   *
+   * @return void
+   */
+  public function register()
+  {
+    //
+  }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+  /**
+   * Bootstrap any application services.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+    Schema::defaultStringLength(191);
+
+    // configuração para alterar a resource das rotas
+    // https://www.youtube.com/watch?v=_fDd_RKKWzk
+    Route::resourceVerbs([
+      'create' => 'novo',
+      'edit' => 'editar'
+    ]);
+  }
 }
